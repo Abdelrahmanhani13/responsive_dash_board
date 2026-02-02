51db97ffaf0818590df91f7a88c760471138eba5
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-02 18:13:34 +0200

    create dashboard view

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AdaptiveLayoutWidget(
+      mobileLayout: (context) => SizedBox(),
+      tabletLayout: (context) => SizedBox(),
+      desktopLayout:(context) => DashboardDesktopLayout(),
+    );
   }
 }
