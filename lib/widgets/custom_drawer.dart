b81da300e6a6b546de4dd18312058d51a232353b
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-02 19:20:53 +0200

    create custom drawer and add first item

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_assets.dart';
+import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: const [
+          SizedBox(height: 50),
+          Card(
+            elevation: 0,
+            color: Color(0xffFAFAFA),
+            child: UserInfoListTile(
+              image: Assets.imagesAvatar2,
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
