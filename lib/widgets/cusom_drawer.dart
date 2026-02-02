e0b137ebfa948a626e490db8280fccbbb41edaa7
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-02 19:03:29 +0200

    create custom drawer

---
+++ lib/widgets/cusom_drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CusomDrawer extends StatelessWidget {
+  const CusomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(children: [
+
+        ],
+      ),
+    );
+  }
+}
