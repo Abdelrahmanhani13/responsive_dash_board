34bfba7ecccc6655fbf2c6c365b76af7d8415adb
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-06 03:23:31 +0200

    create quick invoice part

---
+++ lib/widgets/quick_invoice.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const QuickInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(children: [QuickInvoiceHeader()]),
+    );
+  }
+}
