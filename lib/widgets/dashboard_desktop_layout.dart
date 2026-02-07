4d87fc2db8f2a846c4140b5341adc56e44f62974
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-07 20:54:14 +0200

    finish card

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoke_section.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/my_card.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -27,16 +28,7 @@ class DashboardDesktopLayout extends StatelessWidget {
                       ),
                     ),
                     SizedBox(width: 24),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 40),
-                          // MyCardsAndTransctionHistorySection(),
-                          SizedBox(height: 24),
-                          // Expanded(child: IncomeSection()),
-                        ],
-                      ),
-                    ),
+                    Expanded(child: MyCard()),
                   ],
                 ),
               ),
--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dash_board/utils/app_assets.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -10,14 +12,54 @@ class MyCard extends StatelessWidget {
       aspectRatio: 420 / 215,
       child: Container(
         decoration: ShapeDecoration(
-          image: DecorationImage(
-            image: AssetImage(Assets.imagesMyCard),
-            fit: BoxFit.cover,
-          ),
-          color: Colors.white,
+          image: DecorationImage(image: AssetImage(Assets.imagesMyCard)),
+          color: Color(0xff4EB7F2),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
+        ),
+        child: Column(
+          children: [
+            Material(
+              color: Colors.transparent,
+              child: ListTile(
+                title: Text(
+                  'Card Name',
+                  style: AppStyles.styleRegular16.copyWith(color: Colors.white),
+                ),
+                subtitle: Text(
+                  'Syah Bandi',
+                  style: AppStyles.styleMedium20.copyWith(color: Colors.white),
+                ),
+                trailing: SvgPicture.asset(Assets.imagesGallery),
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  '**** **** **** 1234',
+                  style: AppStyles.styleSemiBold24.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'Exp. Date 12/24',
+                  style: AppStyles.styleRegular14.copyWith(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: 54 - 28),
+          ],
         ),
       ),
     );
