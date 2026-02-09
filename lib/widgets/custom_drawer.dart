1aecd3aace1f61badcd2c9d7c13fa3d535a6ca90
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-09 20:46:58 +0200

    add first item for custom drawer (user info )

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/user_info_model.dart';
+import 'package:responsive_dash_board/utils/app_assets.dart';
+import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white, child: Column());
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          UserInfoListTile(
+            userInfoModel: UserInfoModel(
+              title: 'Lekan Okeowo',
+              subtitle: 'demo@gmail.com',
+              image: Assets.imagesAvatar1,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
