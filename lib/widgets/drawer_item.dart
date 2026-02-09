aecd0ccf7f35e05175b1e9a0a5f25ec044399709
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-09 21:30:18 +0200

    finish logic for drawer list items

--- lib/widgets/drawer_item.dart
+++ lib/widgets/drawer_item.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
-import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/active_and_inactive_drawer_item.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.drawerItemModel});
+  const DrawerItem({
+    super.key,
+    required this.drawerItemModel,
+    required this.isSelected,
+  });
 
   final DrawerItemModel drawerItemModel;
+  final bool isSelected;
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: ListTile(
-        leading: SvgPicture.asset(drawerItemModel.image),
-        title: Text(drawerItemModel.title, style: AppStyles.styleBold16),
-      ),
-    );
+    return isSelected
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
---
+++ lib/widgets/drawer_item_list_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/utils/app_assets.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
+
+class DrawerItemListView extends StatefulWidget {
+  const DrawerItemListView({super.key});
+
+  @override
+  State<DrawerItemListView> createState() => _DrawerItemListViewState();
+}
+
+class _DrawerItemListViewState extends State<DrawerItemListView> {
+  final List<DrawerItemModel> items = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
+    DrawerItemModel(image: Assets.imagesMyTransaction, title: 'My Transaction'),
+    DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments'),
+  ];
+
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            if (selectedIndex != index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isSelected: selectedIndex == index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
