69e6cdaa21d30d93015a6dd38e7e5a1e70780316
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-09 19:47:41 +0200

    start again

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/views/dashboard_view.dart';
 
 void main() {
   runApp(const ResponsiveDashBoard());
--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
-import 'package:responsive_dash_board/widgets/dashboard_desktop_layout.dart';
 
-class DashboardView extends StatelessWidget {
-  const DashboardView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AdaptiveLayout(
-      mobileLayout: (context) => SizedBox(),
-      tabletLayout: (context) => SizedBox(),
-      desktopLayout: (context) => DashboardDesktopLayout(),
-    );
-  }
-}
