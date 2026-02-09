0c70303fe6fe68fbc444115e99eaad2d46569f03
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-09 21:11:34 +0200

    add items part for custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/models/user_info_model.dart';
 import 'package:responsive_dash_board/utils/app_assets.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
+  static const List<DrawerItemModel> items = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
+    DrawerItemModel(image: Assets.imagesMyTransaction, title: 'My Transaction'),
+    DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,6 +27,18 @@ class CustomDrawer extends StatelessWidget {
               subtitle: 'demo@gmail.com',
               image: Assets.imagesAvatar1,
             ),
+          ),
+          SizedBox(height: 8),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: DrawerItem(drawerItemModel: items[index]),
+              );
+            },
           ),
         ],
       ),
