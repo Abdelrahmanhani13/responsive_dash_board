fa527c88d94d59da05c7cf78817365f9898dc377
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-10 04:14:51 +0200

    create all expenses item model

---
+++ lib/models/all_expenses_item_model.dart
@@ -0,0 +1,10 @@
+class AllExpensesItemModel {
+  final String title, subTitle, image, price;
+
+  AllExpensesItemModel({
+    required this.title,
+    required this.subTitle,
+    required this.image,
+    required this.price,
+  });
+}
