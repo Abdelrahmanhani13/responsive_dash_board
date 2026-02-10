54673501deb680a685e08279db9942d3634515d1
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-10 06:48:05 +0200

    finish quick invoice

---
+++ lib/widgets/quick_invoice.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/latest_transaction.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice_form.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const QuickInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          QuickInvoiceHeader(),
+          SizedBox(height: 24),
+          LatestTransaction(),
+          SizedBox(height: 24),
+          Divider(height: 48, color: Colors.grey[300]),
+          QuickInvoiceForm(),
+        ],
+      ),
+    );
+  }
+}
