c1cfb928806fefc16d98631a550eeb4626339fbf
Abdelrahmanhani13 <168323442+Abdelrahmanhani13@users.noreply.github.com>
2026-02-10 06:47:20 +0200

    create custom text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -6,17 +6,20 @@ class CustomTextField extends StatelessWidget {
   final String hint;
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: hint,
-        hintStyle: AppStyles.styleRegular16.copyWith(
-          color: const Color(0xFFAAAAAA),
+    return Material(
+      color: Colors.transparent,
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: AppStyles.styleRegular16.copyWith(
+            color: const Color(0xFFAAAAAA),
+          ),
+          fillColor: const Color(0xFFFAFAFA),
+          filled: true,
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(),
         ),
-        fillColor: const Color(0xFFFAFAFA),
-        filled: true,
-        border: buildBorder(),
-        enabledBorder: buildBorder(),
-        focusedBorder: buildBorder(),
       ),
     );
   }
